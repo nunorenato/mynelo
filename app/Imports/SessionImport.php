ebe4816bedb00e70d1b30f87ae43d9f398d73a32
Nuno Ramos <nuno.ramos@nelo.eu>
2024-07-31 17:35:14 +0100

    Bugs

--- app/Imports/SessionImport.php
+++ app/Imports/SessionImport.php
@@ -10,6 +10,27 @@ use Maatwebsite\Excel\Concerns\ToModel;
 class SessionImport implements ToModel
 {
 
+    const SPEED = 0; // troquei de 0 para 8 para ser o raw e não a velocidade suavizada
+    const SPM = 1;
+    const TIMESTAMP = 2;
+    const LAT = 3;
+    const LNG = 4;
+    const ALT = 5;
+    const HEADING = 6;
+    const ACCU = 7;
+    const RAW = 8;
+    const ACCELX = 9;
+    const ACCELY = 10;
+    const ACCELZ = 11;
+    const ROLL = 12;
+    const PITCH = 13;
+    const HEAD2 = 14;
+    const LACCELX = 15;
+    const LACCELY = 16;
+    const LACCELZ = 17;
+    const HEART = 18;
+    const EXT_SENSOR = 19;
+
     /*
 define('SPEED', 0); // troquei de 0 para 8 para ser o raw e não a velocidade suavizada
 define('SPM', 1);
@@ -38,57 +59,52 @@ define('EXT_SENSOR', 19); // vaaka
 
     }
 
-    public function model(array $row)
+    public function model(array $row):SessionData
     {
 
-        $seconds = $row[2] / 1000;
-        $miliseconds = fmod($row[2],1000)/100;
-        if($miliseconds>9)
-            $miliseconds=9;
+        $seconds = $row[self::TIMESTAMP] / 1000;
+        $milliseconds = fmod($row[self::TIMESTAMP], 1000) / 100;
+        if ($milliseconds > 9)
+            $milliseconds = 9;
 
-        if(!empty($row[19]) && is_numeric($row[19]) && $row[19] > 0){
-            $spm = $spm2 = $row[19]; // External sensor
+        if (!empty($row[self::EXT_SENSOR]) && is_numeric($row[self::EXT_SENSOR]) && $row[self::EXT_SENSOR] > 0) {
+            $spm = $spm2 = $row[self::EXT_SENSOR]; // External sensor
+        } else {
+            $spm = $row[self::SPM];
+            $spm2 = $row[self::EXT_SENSOR];
         }
-        else{
-            $spm = $row[0];
-            $spm2 = $row[19];
-        }
 
-        $dps = $row[1]<40?0:$row[0]*60/$row[1]; // SPM tem de ser maior que 40
+        $dps = $spm < 40 ? 0 : $row[self::SPEED] * 60 / $spm; // SPM tem de ser maior que 40
 
-        $accelx = $row[15]==-1?$row[9]:$row[15];
-        $accely = $row[16]==-1?$row[10]:$row[16];
-        $accelz = $row[17]==-1?$row[11]:$row[17];
+        $accelx = $row[self::LACCELX] == -1 ? $row[self::ACCELX] : $row[self::LACCELX];
+        $accely = $row[self::LACCELY] == -1 ? $row[self::ACCELY] : $row[self::LACCELY];
+        $accelz = $row[self::LACCELZ] == -1 ? $row[self::ACCELZ] : $row[self::LACCELZ];
 
-        $heart = !empty($row[18])&&is_numeric($row[18])?$row[18]:0;
+        $heart = !empty($row[self::HEART]) && is_numeric($row[self::HEART]) ? $row[self::HEART] : 0;
 
-        //dd($row);
+//dd($row);
         return new SessionData([
             'trainid' => $this->session->id,
-            'tagtime' => date("Y-m-d H:i:s",$seconds),
-            'decima_segundo' => $miliseconds,
-            'speed' => $row[0],
-            'spm' => $row[0],
-            'pitch' => $row[13],
-            'roll' => $row[13],
-            'heading' => $row[6],
-            'gpsx' => $row[3],
-            'gpxy' => $row[4],
-            'gpsz' => $row[5],
+            'tagtime' => date("Y-m-d H:i:s", $seconds),
+            'decima_segundo' => $milliseconds,
+            'speed' => $row[self::SPEED],
+            'spm' => $spm,
+            'pitch' => $row[self::PITCH],
+            'roll' => $row[self::ROLL],
+            'heading' => $row[self::HEADING],
+            'gpsx' => $row[self::LAT],
+            'gpxy' => $row[self::LNG],
+            'gpsz' => $row[self::ALT],
             'dps' => $dps,
             'accelX' => $accelx,
             'accelY' => $accely,
             'accelZ' => $accelz,
-            'raw' => $row[8],
-            'accu' => $row[7],
-            'head2' => $row[14],
+            'raw' => $row[self::RAW],
+            'accu' => $row[self::ACCU],
+            'head2' => $row[self::HEAD2],
             'heart' => $heart,
             'app_spm' => $spm2,
         ]);
     }
 
-    public function upsertColumns()
-    {
-        return ['CORRF_GUIA', 'CORRF_VALOR_SEM_IVA'];
-    }
 }
