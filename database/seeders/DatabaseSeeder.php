c70c6adfbe5e62988ce9f05fbee07ae060486cab
Nuno Ramos <nuno.ramos@nelo.eu>
2024-10-17 13:09:57 +0100

    Nelo Coach integration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,7 +24,8 @@ class DatabaseSeeder extends Seeder
 
 
         $this->call([
-           MembershipSeeder::class,
+           //MembershipSeeder::class,
+            CoachSeeder::class,
         ]);
 
 
